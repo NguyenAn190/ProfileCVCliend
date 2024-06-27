463a262045334727ce365dd52368d522694c26ae
NguyenAn190 <112260075+NguyenAn190@users.noreply.github.com>
2024-06-27 19:41:46 +0700

    Update page.tsx

--- src/app/(auth)/login-with-provider/page.tsx
+++ src/app/(auth)/login-with-provider/page.tsx
@@ -1,29 +1,47 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import Cookies from 'js-cookie';
 import React, { Suspense } from 'react';
+import { LoadingPage } from '@/components/loading/loading-page';
+import { useToast } from '@/components/ui/use-toast';
 
 function LoginWithProvider() {
   const router = useRouter();
   const searchParams = useSearchParams();
-
+  const [isLoading, setIsLoading] = useState(false)
+  const { toast } = useToast();
   useEffect(() => {
-    const token = searchParams.get('token');
+    try{
+      setIsLoading(true)
+      const token = searchParams.get('token');
 
-    if (token) {
-      // Lưu token vào cookies
-      Cookies.set('access_token', token, { expires: 7 });
-
-      // Chuyển hướng về trang chủ
+      if (token) {
+        // Lưu token vào cookies
+        Cookies.set('access_token', token, { expires: 7 });
+        toast({
+          title: "Đăng nhập thành công",
+          description: "Giờ bạn đã có thể tạo cv của mình!",
+        });
+        // Chuyển hướng về trang chủ
+        router.replace('/');
+      }
+    }catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Lỗi",
+        description: "Gmail không hợp lệ hoặc đã hết hạn!",
+      });
       router.replace('/');
+    }finally {
+      setIsLoading(false)
     }
   }, [searchParams, router]);
 
   return (
     <div>
-      Đang xử lý...
+      <LoadingPage isLoading={isLoading} />
     </div>
   );
 }
